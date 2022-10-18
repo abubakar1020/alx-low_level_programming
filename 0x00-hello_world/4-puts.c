bef7c05112e33e833bfadf901c0fda4abfbd9894
abubakar1020 <azubair1020@gmail.com>
2022-10-17 20:57:27 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,37 +1,19 @@
 /*
-
-      * File: 4-puts.c
-      *
-      *  * Auth: Abubakar Zubair
-      *
-      *   */
-
-
+ * File: 4-puts.c
+ * Auth: Abubakar Zubair
+ */
 
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - Prints "Programming is like building a multilingual
- *
- *   *                puzzle, followed by a new line.
- *
- *    *
- *
- *     * Return: Always 0.
- *
- *      */
-
+ * main - Prints "Programming is like building a multilingual
+ *                    puzzle, followed by a new line. 
+ * Return: Always 0.
+ */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
 
-
-
-			return (0);
-
+		return (0);
 }
