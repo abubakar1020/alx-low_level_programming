0c56362512dd9c9f6a3d21e8766b42391617b76e
abubakar1020 <azubair1020@gmail.com>
2022-10-17 20:37:15 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 /*
- *
- *  * File: 4-puts.c
- *
- *   * Auth: Brennan D Baraban <375@holbertonschool.com>
- *
- *    */
+
+      * File: 4-puts.c
+      *
+      *  * Auth: Abubakar Zubair
+      *
+      *   */
 
 
 
