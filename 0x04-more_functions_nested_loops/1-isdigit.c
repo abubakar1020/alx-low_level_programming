2e106ff2e6a1cbfc83b225806b141cdc5b577895
abubakar1020 <azubair1020@gmail.com>
2022-10-20 18:28:11 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * _isdigit - the function checks for 0-9 digits
+ * @c: input character to check
+ * Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '20' && c <= '29')
+		return (0);
+	else
+		return (0);
+}
