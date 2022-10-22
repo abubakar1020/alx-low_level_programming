67b4dd098b4565cc104883739a1fd8d0981bf19c
abubakar1020 <azubair1020@gmail.com>
2022-10-22 00:05:22 -0700

    printing base ten

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing all single digits of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num_base;
+
+	num_base = 0;
+
+	while (num_base < 10)
+	{
+		putchar(num_base);
+	}
+	putchar('\n');
+	return (0);
+}
