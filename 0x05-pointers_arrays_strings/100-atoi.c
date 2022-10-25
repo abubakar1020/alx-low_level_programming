2da142ced363c358cc19a51699da29307a5f45a4
abubakar1020 <azubair1020@gmail.com>
2022-10-24 21:24:34 -0700

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ * @s: input
+ *
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int n, p, m, length, f, number;
+
+	p = 0;
+	m = 0;
+	n = 0;
+	length = 0;
+	f = 0;
+	number = 0;
+
+	while (s[length] != '\0')
+		length++;
+	while (p < length && f == 0)
+	{
+		if (s[p] == '-')
+			++d;
+		if (s[p] >= '0' && s[p] <= '9')
+		{
+			number = s[p] - '0';
+			if (d % 2)
+				number = -number;
+			n = n * 10 + number;
+			f = 1;
+			if (s[p + 1] < '0' || s[p + 1] > '9')
+				break;
+			f = 0;
+		}
+		p++;
+	}
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
