5f9811a71d0da0f3a9ce6a585d8bb5dab246c629
abubakar1020 <azubair1020@gmail.com>
2022-10-26 22:28:23 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -program that prints _putchar, followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('putchar');
+	putchar('\n');
+	return (0);
+}
